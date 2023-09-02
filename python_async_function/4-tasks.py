9ab94a342c4ff9eb9e65fd50e964120ef300afe5
Franco <francorr240@gmail.com>
2023-09-02 06:55:22 -0300

    Corrected function name

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,7 +4,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def task_wait_random(n: int, max_delay: int) -> List[float]:
+def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Returns a list of new tasks """
     float_list: List[float] = []
     for i in range(n):
