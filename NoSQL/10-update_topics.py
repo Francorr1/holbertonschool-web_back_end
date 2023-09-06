33b3745c8787af65146fa0b5c0a5da2c60e9010b
Franco <francorr240@gmail.com>
2023-09-06 01:42:54 -0700

    Task 10 complete

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Updates topics """
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Updates topics """
+    query = {"name": name}
+    values = {"$set": {"topics": topics}}
+    return mongo_collection.updateMany(query, values)
