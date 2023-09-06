546d4cc38c2e2b0b89a65cdb59f19d9729ee9407
Franco <francorr240@gmail.com>
2023-09-06 01:33:09 -0700

    Task 8 complete

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Lists all documents in a collection """
+import pymongo
+
+
+def list_all(mongo_collection):
+    """ Lists all documents in a collection """
+    return mongo_collection.find()
