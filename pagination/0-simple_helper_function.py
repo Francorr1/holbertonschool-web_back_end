2c85e267d52e1384cd51da76f319bbf4059427ce
Franco <francorr240@gmail.com>
2023-11-15 20:17:51 -0300

    Tasks 0 and 2 complete

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""simple_helper_function module"""
+
+
+def index_range(page, page_size):
+    """ Return start and end index """
+    last_index = page * page_size
+    first_index = last_index - page_size
+    return (first_index, last_index)
